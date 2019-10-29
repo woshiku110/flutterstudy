338e9e8e91054a3812db78fcdeb785c178b9dee9
周明 <woshiku112@gmail.com>
2019-10-29 17:47:55 +0800

    加入路由管理页面

---
+++ lib/route/route.dart
@@ -0,0 +1,16 @@
+import 'package:fluro/fluro.dart';
+import 'package:flutterstudy/pages/home.dart';
+import 'package:flutterstudy/pages/test.dart';
+class Routes{
+    static Router router;
+    static void configureRoutes(Router router){
+        //主页
+        router.define('/', handler:  new Handler(handlerFunc:(context,params){
+            return new Home(title: 'homeTest',);
+        } ));
+        router.define('/test', handler:  new Handler(handlerFunc:(context,params){
+            return new Test(title: 'testTest',);
+        } ));
+        Routes.router = router;
+    }
+}
